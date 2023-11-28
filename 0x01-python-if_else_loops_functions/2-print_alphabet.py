e2fc6aef38a7a9075ae223dbf0a8f99c8af20ded
Tony <tonymusyoka11@gmail.co>
2023-11-28 05:46:55 -0800

    2-print_alphabet.py

--- 0x01-python-if_else_loops_functions/2-print_alphabet.py
+++ 0x01-python-if_else_loops_functions/2-print_alphabet.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z')+1): print(chr(i), end="" if chr(i) != 'e' and chr(i) != 'q' else "")
+for i in range(ord('a'), ord('z')+1):
+    print(chr(i), end="" if chr(i) not in 'e' and chr(i) not in 'q' else "")
 
