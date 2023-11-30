1617be0419677ee8d205144003bffe85ad66e622
Tony <tonymusyoka11@gmail.co>
2023-11-30 01:30:46 -0800

    101-magic_calculation.py

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,28 @@
 #include "lists.h"
 
 /**
- * check_cycle - checks if a linked list contains a cycle
- * @list: linked list to check
- *
- * Return: 1 if the list has a cycle, 0 if it doesn't
+ * check_cycle - checks if a linked list has a cycle
+ * @list: pointer to the head of the linked list
+ * Return: 0 if no cycle, 1 if a cycle is present
  */
 int check_cycle(listint_t *list)
 {
-        listint_t *slow = list;
-        listint_t *fast = list;
+listint_t *slow, *fast;
 
-        if (!list)
-                return (0);
+if (list == NULL || list->next == NULL)
+return (0);
 
-        while (slow && fast && fast->next)
-        {
-                slow = slow->next;
-                fast = fast->next->next;
-                if (slow == fast)
-                        return (1);
-        }
+slow = list;
+fast = list->next;
 
-        return (0);
+while (fast != NULL && fast->next != NULL)
+{
+if (slow == fast)
+return (1);
+
+slow = slow->next;
+fast = fast->next->next;
 }
 
+return (0);
+}
